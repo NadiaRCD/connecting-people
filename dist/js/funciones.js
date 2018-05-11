5d696ff20210be21e0a75cac58d2727da1126580
CarlaZumaran <carla.z@rompecabeza.cl>
2018-05-11 12:08:30 -0300

    Merch de carpetas y archivos, anexo de lib de fuentes

--- dist/js/funciones.js
+++ dist/js/funciones.js
@@ -8,3 +8,13 @@ $("li[class^='list-button']").click(function() {
     $('.sidebar_background').removeClass('active')
 })
 
+/* Acordeon */
+
+$('.accordion').on('click','.accordion-header',function(){
+    var t = $(this);
+    t.toggleClass('active-header')
+      var tp = t.next();
+      var p = t.parent().siblings().find('.accordion-body');
+      tp.slideToggle();
+    p.slideUp();
+  });
